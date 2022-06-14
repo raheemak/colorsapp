0c8b22f756c3aca6357298c70b40c21b89603dba
Raheema Kadwa <rkadwa@fordham.edu>
2022-06-13 22:32:14 -0400

    updated authorization

--- client/src/store/APIProvider.js
+++ client/src/store/APIProvider.js
@@ -3,21 +3,33 @@ import APIContext from "./APIContext"
 
 
 
-const APIProvider = (props) => {
+const APIProvider =  (props) => {
 
     //set api url based on current url 
     var url = ""
-    console.log (window.location.href)
-    if (window.location.href.includes ("localhost")){
+    console.log(window.location.href)
+    if (window.location.href.includes("localhost")) {
         url = "http://localhost:3001"
-    }else {//will be production
+    } else {//will be production
         url = "https://colorsappraheema.herokuapp.com"
     }
 
-    const apiContext = {
-        api_url: url ,
-        authorization: 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjZ3TTZRV2w5dHVLM3pLcGFWLWNXNiJ9.eyJpc3MiOiJodHRwczovL2Rldi1uemU1dmZ4eS51cy5hdXRoMC5jb20vIiwic3ViIjoiTGJDTmJ3cGZ1VEtOdlVPT09UUlhOZEYzUk1SejZIZjNAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vY29sb3JzYXBwcmFoZWVtYS5oZXJva3VhcHAuY29tLyIsImlhdCI6MTY1NTA4MDE1MSwiZXhwIjoxNjU1MTY2NTUxLCJhenAiOiJMYkNOYndwZnVUS052VU9PT1RSWE5kRjNSTVJ6NkhmMyIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.SQyWdRhREVBr3TH33ikIKghXE81xGf9bDPmS7Bd4m9LiSWUCR9TI2N1i6Xq3JL6jdTcoQn0gEq092N7PRMqJXQutg5KZrsOVobXv49qMoW-_7lHPEWQnTZHgtHvHzXy2eUKmm5WhtbyAGZep_ic3DwVk_slXXC_2nFa4J7NZitGJ-8VItGwbICEjMnYaDe6qzicnpinduX8kn6UznajMWoOsnOUm8b7VIUBB4mTVWPQIfU3YnS8qQ5-tDFwb0jdIUHyeE_2H1Y1U3YH_Yr7Kwd-747nOSUOoeThv1ZNwYi8t330NUUKHLall8rhPuvKZgmRSwxmNwwS2l73DuT4T3w'
 
+    var options = {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: '{"client_id":"LbCNbwpfuTKNvUOOOTRXNdF3RMRz6Hf3","client_secret":"omHXxXQ5qxeK4LY8jFrX9_YoibC6OOACKsjx2vApMlAw4gYUgR8pM1XSG4BlYbu2","audience":"https://colorsappraheema.herokuapp.com/","grant_type":"client_credentials"}'
+    };
+
+    var authorizationToken=""
+    fetch("https://dev-nze5vfxy.us.auth0.com/oauth/token", options)
+    .then ((res)=>res.json())    
+    .then ((res)=>{authorizationToken = res.access_token; })
+            
+
+    const apiContext = {
+        api_url: url,
+        authorization: `Bearer ${authorizationToken}`
     }
 
     return (
